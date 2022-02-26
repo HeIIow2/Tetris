1a736a60ae76c4670ff19ca8f6bdb942395555b1
HeIIow2 <Lars.Noack@outlook.de>
2022-02-26 23:03:07 +0100

    addet game over

--- tetris.py
+++ tetris.py
@@ -144,6 +144,15 @@ class Grid:
                 row.append(Cell())
             self.grid.append(row)
 
+    def reset_grid(self):
+        self.grid = []
+
+        for i in range(self.grid_height):
+            row = []
+            for j in range(self.grid_width):
+                row.append(Cell())
+            self.grid.append(row)
+
     def game_over(self):
         print("game over")
         exit(666)
@@ -209,13 +218,6 @@ class Grid:
         for i in to_pop:
             self.figures.pop(i)
 
-        # wenn in der obersten Zeile ein block ist, game over
-        first_row = self.grid[0]
-        for cell in first_row:
-            if cell.mode != 0:
-                self.game_over()
-                break
-
         # Wenn eine Zeile voll ist, lösche diese
         number_of_full_row = 0
         for i, row in enumerate(self.grid):
@@ -228,7 +230,14 @@ class Grid:
                 number_of_full_row += 1
                 self.remove_row(i)
 
-        return number_of_full_row, continuous_soft_drop
+        # wenn in der obersten Zeile ein block ist, game over
+        first_row = self.grid[0]
+        game_over_ = False
+        for cell in first_row:
+            if cell.mode != 0:
+                game_over_ = True
+
+        return number_of_full_row, continuous_soft_drop, game_over_
 
     def spawn_figure(self, figure: Figure):
         figure = copy.deepcopy(figure)
@@ -387,19 +396,24 @@ class Game:
         self.ui_frame = ui_frame
         self.ui_frame.bind('<KeyPress>', self.on_key_press)
 
+        self.status_label = tk.Label(self.ui_frame, text="Level 1", font=('font-family font-size style', 25))
+        self.status_label.grid(row=0, column=0, columnspan=3)
+
         self.queue_label = tkinter.Label(self.ui_frame)
         self.queue_image = None
         self.render_queue()
-        self.queue_label.grid(row=0, column=0)
+        self.queue_label.grid(row=1, column=0)
 
         self.img_label = tkinter.Label(self.ui_frame)
         self.img = None
         self.render()
-        self.img_label.grid(row=0, column=1)
+        self.img_label.grid(row=1, column=1)
 
         self.description_frame = tk.Frame(root)
-        self.description = Description(self.description_frame, [("level", level), ("score", start_score), ("speed", start_speed), ("rows", 0)])
-        self.description_frame.grid(row=0, column=2)
+        self.description = Description(self.description_frame, [("level", level), ("score", start_score), ("speed", start_speed), ("rows", "0/10")])
+        self.description_frame.grid(row=1, column=2)
+
+        self.game_over = False
 
         self.score = start_score
         self.level = level
@@ -407,6 +421,16 @@ class Game:
         self.speed = start_speed
 
         self.broken_lines = 0
+
+    def reset_level(self):
+        self.score = 0
+        self.level = 1
+        self.broken_lines = 0
+
+        self.status_label.config(text="Level: 1")
+
+        self.description.set_element("score", self.score)
+        self.update_level()
 
     def set_speed(self):
         # https://gaming.stackexchange.com/questions/13057/tetris-difficulty#13129
@@ -457,15 +481,20 @@ class Game:
         if level > self.level_cap:
             level = self.level_cap
         required_lines = level * 10
-        print(f"update level {required_lines}, {self.broken_lines}")
+
+        self.description.set_element("rows", f"{self.broken_lines}/{required_lines}")
 
         if self.broken_lines >= required_lines:
             self.broken_lines -= required_lines
             self.level += 1
             self.description.set_element("level", self.level)
+            self.status_label.config(text=f"Level: {level}")
             self.set_speed()
 
     def on_key_press(self, e):
+        if self.game_over:
+            self.update(resume=True)
+
         if e.keycode == 68 or e.keycode == 39:
             self.grid.right()
             self.render()
@@ -488,7 +517,6 @@ class Game:
 
         if e.keycode == 40 or e.keycode == 83 or e.keycode == 32:
             self.update(soft_drop=True)
-            self.render()
             return
 
     def render(self):
@@ -499,8 +527,16 @@ class Game:
         self.queue_image = ImageTk.PhotoImage(self.queue.draw(20, 20, 1))
         self.queue_label.config(image=self.queue_image)
 
-    def update(self, soft_drop=False):
-        full_rows, continuous_soft_drop = self.grid.update(soft_drop=soft_drop)
+    def update(self, resume=False, soft_drop=False):
+        if self.game_over:
+            if resume:
+                self.grid.reset_grid()
+                self.reset_level()
+                self.game_over = True
+            else:
+                return
+
+        full_rows, continuous_soft_drop, self.game_over = self.grid.update(soft_drop=soft_drop)
         self.render()
 
         if full_rows:
@@ -509,13 +545,17 @@ class Game:
 
         self.score += self.get_score(full_rows) + continuous_soft_drop
         self.description.set_element("score", self.score)
-        self.description.set_element("rows", self.broken_lines)
 
         if self.grid.allow_spawn():
             self.grid.spawn_figure(self.queue.next_piece())
             self.render_queue()
 
         print(f"cycle: {self.cycle}; score: {self.score}; level: {self.level}; speed: {self.speed}")
+
+        if self.game_over:
+            self.status_label.config(text="Game Over")
+            return
+
         self.cycle += 1
 
 
