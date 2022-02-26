5eccfdb5ede13e841de6fd3c786bf14811709479
HeIIow2 <Lars.Noack@outlook.de>
2022-02-26 17:16:03 +0100

    addet a queue with preview

--- tetris.py
+++ tetris.py
@@ -97,6 +97,38 @@ class Figure:
 
         self.grid = temp_grid
 
+    def draw(self, grid_width: int, width: int, height: int, spacing: int):
+        img_width = width * grid_width + spacing * (grid_width + 1)
+        img_height = height * self.height + spacing * (self.height + 1)
+
+        margin_left = int((grid_width-self.width)/2)
+        margin_right = margin_left + (grid_width-self.width)%2
+
+        img = Image.new("RGB", (img_width, img_height), color="#666")
+
+        y = spacing
+        for i in range(self.height):
+            x = spacing
+            for n in range(margin_left):
+                Image.Image.paste(img, Cell().draw(width, height), (x, y))
+                x += spacing + width
+
+            for j in range(self.width):
+                if self.grid[i][j] is None:
+                    Image.Image.paste(img, Cell().draw(width, height), (x, y))
+                else:
+                    Image.Image.paste(img, self.grid[i][j].draw(width, height), (x, y))
+                x += spacing + width
+
+            for n in range(margin_right):
+                Image.Image.paste(img, Cell().draw(width, height), (x, y))
+                x += spacing + width
+
+            y += spacing + height
+
+        return img
+
+
 
 class Grid:
     def __init__(self, width: int, height: int):
@@ -269,8 +301,9 @@ class Grid:
 class Description:
     def __init__(self, root, elements: list):
         self.elements = {}
-        for i, element in enumerate(elements):
-            self.elements[element] = tkinter.Label(root, text=f"{element}: ")
+        for i, element_ in enumerate(elements):
+            element, value = element_
+            self.elements[element] = tkinter.Label(root, text=f"{element}: {value}")
             self.elements[element].grid(row=i, column=0)
 
     def set_element(self, element, value):
@@ -280,31 +313,101 @@ class Description:
         self.elements[element].config(text=f"{element}: {value}")
 
 
+class Queue:
+    def __init__(self, length: int, height: int, width: int):
+        self.grid = []
+
+        self.grid_height = height
+        self.grid_width = width
+
+        for i in range(height):
+            row = []
+            for j in range(width):
+                row.append(Cell())
+            self.grid.append(row)
+
+        self.prev_piece_ind = -1
+
+        self.queue = []
+        for i in range(length):
+            self.queue.append(self.get_random_piece())
+
+    def get_random_piece(self):
+        # https://gaming.stackexchange.com/questions/13057/tetris-difficulty#13129
+
+        FIGURES = [
+            Figure(4, [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], 1),
+            Figure(3, [1, 0, 0, 1, 1, 1, 0, 0, 0], 2),
+            Figure(3, [0, 0, 1, 1, 1, 1, 0, 0, 0], 3),
+            Figure(2, [1, 1, 1, 1], 4),
+            Figure(3, [0, 1, 1, 1, 1, 0, 0, 0, 0], 5),
+            Figure(3, [0, 1, 0, 1, 1, 1, 0, 0, 0], 6),
+            Figure(3, [1, 1, 0, 0, 1, 1, 0, 0, 0], 7),
+        ]
+
+        random_number = random.randint(0, 7)
+        if random_number == self.prev_piece_ind or random_number == 7:
+            random_number = random.randint(0, 6)
+
+        self.prev_piece_ind = random_number
+        return FIGURES[random_number]
+
+    def next_piece(self):
+        next_piece_ = self.queue[0]
+        self.queue.pop(0)
+
+        self.queue.append(self.get_random_piece())
+        return next_piece_
+
+    def draw(self, width: int, height: int, spacing: int):
+        img_height = 0
+        figure_images = []
+        for figure in self.queue:
+            figure_images.append(figure.draw(grid_width=4, width=width, height=height, spacing=spacing))
+            img_height += figure_images[-1].height
+
+        img_width = width * 4 + spacing * (4 + 1)
+
+        img = Image.new("RGB", (img_width, img_height), color="#666")
+        current_height = 0
+        for image in figure_images:
+            img.paste(image, (0, current_height))
+
+            current_height += image.height
+
+        return img
+
+
 class Game:
     def __init__(self, ui_frame, queue_len=5, width=10, height=20, level=1, start_speed=800, start_score=0, level_cap=20):
         self.queue_len = queue_len
         self.cycle = 0
 
         self.grid = Grid(width=width, height=height)
+        self.queue = Queue(queue_len, width=4, height=height)
 
         self.ui_frame = ui_frame
         self.ui_frame.bind('<KeyPress>', self.on_key_press)
 
+        self.queue_label = tkinter.Label(self.ui_frame)
+        self.queue_image = None
+        self.render_queue()
+        self.queue_label.grid(row=0, column=0)
+
         self.img_label = tkinter.Label(self.ui_frame)
         self.img = None
         self.render()
-        self.img_label.grid(row=0, column=0)
+        self.img_label.grid(row=0, column=1)
 
         self.description_frame = tk.Frame(root)
-        self.description = Description(self.description_frame, ["level", "score", "speed", "rows"])
-        self.description_frame.grid(row=0, column=1)
+        self.description = Description(self.description_frame, [("level", level), ("score", start_score), ("speed", start_speed), ("rows", 0)])
+        self.description_frame.grid(row=0, column=2)
 
         self.score = start_score
         self.level = level
         self.level_cap = level_cap
         self.speed = start_speed
 
-        self.prev_piece_ind = -1
         self.broken_lines = 0
 
     def set_speed(self):
@@ -364,26 +467,6 @@ class Game:
             self.description.set_element("level", self.level)
             self.set_speed()
 
-    def get_next_piece(self):
-        # https://gaming.stackexchange.com/questions/13057/tetris-difficulty#13129
-
-        FIGURES = [
-            Figure(4, [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], 1),
-            Figure(3, [1, 0, 0, 1, 1, 1, 0, 0, 0], 2),
-            Figure(3, [0, 0, 1, 1, 1, 1, 0, 0, 0], 3),
-            Figure(2, [1, 1, 1, 1], 4),
-            Figure(3, [0, 1, 1, 1, 1, 0, 0, 0, 0], 5),
-            Figure(3, [0, 1, 0, 1, 1, 1, 0, 0, 0], 6),
-            Figure(3, [1, 1, 0, 0, 1, 1, 0, 0, 0], 7),
-        ]
-
-        random_number = random.randint(0, 7)
-        if random_number == self.prev_piece_ind or random_number == 7:
-            random_number = random.randint(0, 6)
-
-        self.prev_piece_ind = random_number
-        return FIGURES[random_number]
-
     def on_key_press(self, e):
         if e.keycode == 68 or e.keycode == 39:
             self.grid.right()
@@ -414,6 +497,10 @@ class Game:
         self.img = ImageTk.PhotoImage(self.grid.draw(20, 20, 1))
         self.img_label.config(image=self.img)
 
+    def render_queue(self):
+        self.queue_image = ImageTk.PhotoImage(self.queue.draw(20, 20, 1))
+        self.queue_label.config(image=self.queue_image)
+
     def update(self, soft_drop=False):
         full_rows, continuous_soft_drop = self.grid.update(soft_drop=soft_drop)
         self.render()
@@ -423,11 +510,12 @@ class Game:
             self.update_level()
 
         self.score += self.get_score(full_rows) + continuous_soft_drop
-        self.description.set_element("score",self.score)
-        self.description.set_element("rows",self.broken_lines)
+        self.description.set_element("score", self.score)
+        self.description.set_element("rows", self.broken_lines)
 
         if self.grid.allow_spawn():
-            self.grid.spawn_figure(self.get_next_piece())
+            self.grid.spawn_figure(self.queue.next_piece())
+            self.render_queue()
 
         print(f"cycle: {self.cycle}; score: {self.score}; level: {self.level}; speed: {self.speed}")
         self.cycle += 1
